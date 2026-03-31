a64dd44306fc9393baea7165cf9f9b0845033c10
wubbleramble <gustavo.ribeiro.5999@gmail.com>
2026-03-31 00:58:34 -0300

    Update controlnet.py

--- script/controlnet.py
+++ script/controlnet.py
@@ -293,7 +293,7 @@ def Download_Model(b):
         CD(HOME)
 
 def load_css():
-    if SM or not Path(CSSCN).exists(): SyS(f'curl -sLo {CSSCN} https://github.com/gutris1/segsmaker/raw/main/script/controlnet.css')
+    if SM or not Path(CSSCN).exists(): SyS(f'curl -sLo {CSSCN} https://github.com/wubbleramble/segsmaker_mine/raw/main/script/controlnet.css')
     display(HTML(f'<style>{Path(CSSCN).read_text()}</style>'))
 
 load_css()
